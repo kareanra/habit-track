e5a4e1e94a0120f22f11431fcf1ae22b65891b89
Kyle Arean-Raines <kyle@toasttab.com>
2020-02-25 07:49:05 -0500

    Use dummy answer views

--- app/src/main/java/com/kareanra/habittrack/view/GraphViewActivity.kt
+++ app/src/main/java/com/kareanra/habittrack/view/GraphViewActivity.kt
@@ -15,15 +15,21 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.formatter.LargeValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.kareanra.habittrack.R
+import com.kareanra.habittrack.model.InputType
+import com.kareanra.habittrack.model.view.AnswerView
+import com.kareanra.habittrack.model.view.AnswerableHabitView
+import com.kareanra.habittrack.model.view.HabitView
 import kotlinx.android.synthetic.main.activity_graph.*
 import kotlin.random.Random
 
 class GraphViewActivity : AppCompatActivity(), OnChartValueSelectedListener {
 
     private lateinit var tfLight: Typeface
+
+    private val habitsByEntry = mutableMapOf<Entry, AnswerableHabitView>()
+
     private var dialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,7 +60,7 @@ class GraphViewActivity : AppCompatActivity(), OnChartValueSelectedListener {
         }
 
         val dayRange = Day(DayOfWeek.SUNDAY, 23)..Day(DayOfWeek.SATURDAY, 29)
-        val todayDayOfWeek = DayOfWeek.MONDAY
+        val todayDayOfWeek = dayRange.random().dayOfWeek
 
         habit_chart.xAxis.apply {
             typeface = tfLight
@@ -85,14 +91,21 @@ class GraphViewActivity : AppCompatActivity(), OnChartValueSelectedListener {
 
         habit_chart.axisRight.isEnabled = false
 
+        graph_today_button.setOnClickListener {
+            loadData(dayRange)
+        }
+
         loadData(dayRange)
     }
 
     override fun onValueSelected(e: Entry, h: Highlight) {
         dismissDialog()
+
+        val message = habitsByEntry.getValue(e).answer!!.notes
+
         dialog = AlertDialog.Builder(this)
-            .setTitle("Notes for today")
-            .setMessage("$e - $h")
+            .setTitle("Notes for habit")
+            .setMessage(message)
             .setPositiveButton("Done") { _, _ ->
                 dismissDialog()
             }
@@ -112,35 +125,58 @@ class GraphViewActivity : AppCompatActivity(), OnChartValueSelectedListener {
     }
 
     private fun loadData(days: List<Day>) {
-        val values1 = mutableListOf<BarEntry>()
-        val values2 = mutableListOf<BarEntry>()
-        val values3 = mutableListOf<BarEntry>()
-        val values4 = mutableListOf<BarEntry>()
+        val habits = listOf(
+            HabitView(
+                id = 0,
+                name = "How many cups of coffee?"
+            ),
+            HabitView(
+                id = 1,
+                name = "How anxious?"
+            ),
+            HabitView(
+                id = 2,
+                name = "How elevated?"
+            ),
+            HabitView(
+                id = 3,
+                name = "How depressed?"
+            )
+        )
 
-        val randomMultiplier = 10f
+        val dates = listOf(20200223, 20200224, 20200225, 20200226, 20200227, 20200228, 20200229)
 
-        for (day in days) {
-            values1.add(BarEntry(day.dayOfWeek.ordinal.toFloat(), Random.nextFloat() * randomMultiplier))
-            values2.add(BarEntry(day.dayOfWeek.ordinal.toFloat(), Random.nextFloat() * randomMultiplier))
-            values3.add(BarEntry(day.dayOfWeek.ordinal.toFloat(), Random.nextFloat() * randomMultiplier))
-            values4.add(BarEntry(day.dayOfWeek.ordinal.toFloat(), Random.nextFloat() * randomMultiplier))
+        val habitsWithAnswers = habits.map { habit ->
+            (0..6).map {
+                AnswerableHabitView(
+                    habit = habit,
+                    inputType = InputType.RANGE,
+                    answer = AnswerView(
+                        yyyymmdd = dates[it],
+                        value = Random.nextInt(11),
+                        notes = "Some random notes go here ($it)"
+                    )
+                )
+            }
         }
 
-        val set1: IBarDataSet
-        val set2: IBarDataSet
-        val set3: IBarDataSet
-        val set4: IBarDataSet
+        val dataSets = habitsWithAnswers.map {
+            BarDataSet(
+                it.map { ans ->
+                    BarEntry(
+                        days[dates.indexOf(ans.answer!!.yyyymmdd)].dayOfWeek.ordinal.toFloat(), // TODO clean up
+                        ans.answer.value.toFloat()
+                    ).also { e ->
+                        habitsByEntry[e] = ans
+                    }
+                },
+                it.first().habit.name
+            ).apply {
+                color = Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)) // TODO
+            }
+        }
 
-        set1 = BarDataSet(values1, "Habit 1")
-        set1.color = Color.rgb(104, 241, 175)
-        set2 = BarDataSet(values2, "Habit 2")
-        set2.color = Color.rgb(164, 228, 251)
-        set3 = BarDataSet(values3, "Habit 3")
-        set3.color = Color.rgb(242, 247, 158)
-        set4 = BarDataSet(values4, "Habit 4")
-        set4.color = Color.rgb(255, 102, 0)
-
-        val data = BarData(set1, set2, set3, set4)
+        val data = BarData(dataSets)
         data.setValueFormatter(LargeValueFormatter())
         data.setValueTypeface(tfLight)
         habit_chart.data = data
